f2400a68ab728ce25e087b2711343fc745ec0b78
Lank206 <gerardojoshuapina0826@gmail.com>
2026-03-07 20:38:17 -0600

    Flask terminado

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
+EXPOSE 5001
 
 CMD ["python","run.py"]
--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,26 @@ main = Blueprint("main", __name__)
 def index():
     return render_template("index.html")
 
+@main.route("/recuperar")
+def recuperar():
+    return render_template("recuperar.html")
+
+@main.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+@main.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+
+@main.route("/envios")
+def envios():
+    return render_template("envios.html")
+
+@main.route("/rastrear")
+def rastrear():
+    return render_template("rastrear.html")
+
+@main.route("/historial")
+def historial():
+    return render_template("historial.html")
